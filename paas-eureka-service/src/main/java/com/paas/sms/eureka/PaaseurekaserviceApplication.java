3f060e1ca447a784001c5ab3cded0ee212f68303
Sandeep <sandeep@gmail.com>
2019-11-06 11:26:34 +0530

    m1 functionality added

--- paas-eureka-service/src/main/java/com/paas/sms/eureka/PaaseurekaserviceApplication.java
+++ paas-eureka-service/src/main/java/com/paas/sms/eureka/PaaseurekaserviceApplication.java
@@ -7,10 +7,16 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class PaaseurekaserviceApplication {
+	
+	public void m1() {
+		System.out.println("I am in m1()");
+	}
 
 	//Added comment
 	public static void main(String[] args) {
 		SpringApplication.run(PaaseurekaserviceApplication.class, args);
 	}
+	
+
 
 }
