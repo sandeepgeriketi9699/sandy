ce6a290294972e8bb2dcc419415264d26829d8bc
Sandeep <sandeep@gmail.com>
2019-11-06 11:23:12 +0530

    "Comment Added"

--- paas-eureka-service/src/main/java/com/paas/sms/eureka/PaaseurekaserviceApplication.java
+++ paas-eureka-service/src/main/java/com/paas/sms/eureka/PaaseurekaserviceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class PaaseurekaserviceApplication {
 
+	//Added comment
 	public static void main(String[] args) {
 		SpringApplication.run(PaaseurekaserviceApplication.class, args);
 	}
