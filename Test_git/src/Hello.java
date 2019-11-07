bb9ff21bac9b36e3ce5b5bc3303426a442a27441
Sandeep <sandeep@gmail.com>
2019-11-07 11:04:47 +0530

    added

---
+++ Test_git/src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+	
+	public String  m1() {
+		return "hello";
+	}
+public static void main (String []args) {
+	Hello h=new Hello();
+	String s=h.m1();
+	System.out.println(s);
+	
+	
+}
+}
